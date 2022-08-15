dbf4e37a35fc798be3d8cf25afe2170c2af5a9b0
blinky1994 <56903269+blinky1994@users.noreply.github.com>
2022-08-15 15:26:50 +0800

    fixes to directories

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ class App extends React.Component {
  
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-       fetch('http://localhost:3001/imageurl', {
+       fetch('https://dry-headland-17016.herokuapp.com/imageurl', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
@@ -90,7 +90,7 @@ class App extends React.Component {
              .then(response => response.json())
              .then((predictObj) => {
                 if (predictObj) {
-                    fetch('http://localhost:3001/image', {
+                    fetch('https://dry-headland-17016.herokuapp.com/image', {
                     method: 'put',
                     headers: {'Content-Type': 'application/json'},
                     body: JSON.stringify({
