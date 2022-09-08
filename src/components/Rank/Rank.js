f1008d4fdc969a455e73edbb8b7fbecfc3392343
blinky1994 <56903269+blinky1994@users.noreply.github.com>
2022-09-08 17:39:44 +0800

    Added rank fetching with aws lambda

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react'
 
-const Rank = ({name, entries}) => {
+export default class Rank extends Component {
+  
+	constructor() {
+		super();
+		this.state = {
+			rank: ''
+		}
+	}
+
+	componentDidMount() {
+		this.generateEmoji(this.props.entries);
+	};
+
+	componentDidUpdate(prevProps, prevState) {
+		if (prevProps.entries !== this.props.entries) {
+			this.generateEmoji(this.props.entries);
+		}
+		
+	}
+
+	generateEmoji = (entries) => {
+		fetch(`https://4onba5y346.execute-api.ap-southeast-1.amazonaws.com/rank?rank=${entries}`)
+		.then(resp => resp.json())
+		.then(data => this.setState({ rank: data.input }))
+		.catch(console.log);
+	}
+
+	render() {
 	return (
-		<div>
-			<div className='white f3'>
-				{`Hi ${name}, your current entry count is...`}
-			</div>
-			<div className='white f1'>
-				{entries}
-			</div>
+	<div>
+		<div className='white f3'>
+		{`Hi ${this.props.name}, your current entry count is...`}
 		</div>
-	);
-}
 
-export default Rank;
+		<div className='white f1'>
+		{this.props.entries}
+		</div>
+
+		<div className='white f3'>
+		{`Rank Badge: ${this.state.rank}`}
+		</div>
+	</div>
+	)
+  }
+}
